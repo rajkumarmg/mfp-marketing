c83371f153542b8ed5adc22464428290047b60fa
Rajkumar Muthuramalingam <muthumgr@yahoo.co.in>
2022-03-07 12:45:35 +0530

    initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Mount function to startup the app
+const mount = (el) => { 
+    ReactDOM.render(
+        <App />, 
+        el
+    );
+};
+
+// if we are in dev env or isolation
+// call mount immediately
+if(process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_marketing-dev-root');
+    if(devRoot) {
+        mount(devRoot);
+    }
+}
+//we are running through container
+//and we shoud import the mount function
+
+export { mount };
